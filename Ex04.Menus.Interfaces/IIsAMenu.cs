f37d9fb4fa89b98fd83af431fe464ef6026ffeda
Itty <itamar.shuval@gmail.com>
2018-05-22 19:31:43 +0300

    started interface

---
+++ Ex04.Menus.Interfaces/IIsAMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Interface
+{
+    internal interface IIsAMenu
+    {
+        public static void Show();
+
+        public MenuItem AddMenuItem(string i_ItemLabel);
+
+        public static void 
+    }
+}
