bab0bbe461bb8ada9e3d1a698edd4b13162c6951
Itty <itamar.shuval@gmail.com>
2018-05-23 14:21:29 +0300

    minor

--- Ex04.Test/InterfaceMain.cs
+++ Ex04.Test/InterfaceMain.cs
@@ -7,7 +7,7 @@ namespace Ex04.Test
     {
         internal static void build()
         {
-            MainMenu firstMainMenu = new MainMenu("Main Menu", 0, null);
+            MainMenu firstMainMenu = new MainMenu("Interface Main Menu", 0, null);
             MainMenu dateAndTimeMenu = firstMainMenu.addNewMenu("Show Data/Time", 1, firstMainMenu);
             dateAndTimeMenu.addNewItemToMenu("Show Time", firstMainMenu, new Time());
             dateAndTimeMenu.addNewItemToMenu("Show Date", firstMainMenu, new Date());
